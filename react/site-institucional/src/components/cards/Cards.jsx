88a19995871f22908bde1847b7303785da6f5cc3
daniloBritoo <danilo.brito@sptech.school>
2024-05-19 17:24:56 -0300

    feat: alterado propriedades do Card

--- react/site-institucional/src/components/cards/Cards.jsx
+++ react/site-institucional/src/components/cards/Cards.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import styles from'./Cards.module.css'; // Arquivo de estilos CSS para o card
+import styles from './Cards.module.css'; // Arquivo de estilos CSS para o card
 
 // Componente de Card
-const Card = ({image, alt, title, description}) => {
+const Card = ({ image, alt, title, description }) => {
   return (
     <div className={styles["card"]}>
-    <div className={styles['line-yellow']}></div>
-    <div className={styles['card-image']}>
-      {image}
-    </div>
+      <div className={styles['line-yellow']}></div>
+      <div className={styles['card-image']}>
+        {image}
+      </div>
       <div className={styles["card-content"]}>
         <h3 className={styles["card-title"]}>{title}</h3>
         <p className={styles["card-description"]}>{description}</p>
