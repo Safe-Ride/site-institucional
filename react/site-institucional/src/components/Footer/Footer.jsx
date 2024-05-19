02795d36eddf0ee87d14b34c0a5961ba47b5619c
daniloBritoo <danilo.brito@sptech.school>
2024-05-19 19:22:55 -0300

    feat: adicionado componente Footer e estrutura da home finalizada sem responsividade

--- react/site-institucional/src/components/Footer/Footer.jsx
+++ react/site-institucional/src/components/Footer/Footer.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import imgLogo from "../../utils/assets/logo.jpg"
+import imgLogo from "../../utils/assets/logo.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarker, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
     return (
-        <div className={styles["header"]}>
-        <div className={styles["container"]}>
+        <div className={styles["footer"]}>
+            <div className={styles["container"]}>
+                <div className={styles["row"]}>
+                    <div className={styles["logo"]}>
+                        <img src={imgLogo} alt="Logo" />
+                    </div>
+
+                    <div className={styles["contato"]}>
+                        <h3 className={styles["text-uppercase"]}>
+                            <strong>Contato</strong>
+                        </h3>
+                        <p>
+                            <FontAwesomeIcon icon={faMapMarker} />
+                            &nbsp; Rua Haddock Lobo, 595 -
+                            Cerqueira César, São Paulo - SP, 01414-001
+                        </p>
+                        <p>
+                            <FontAwesomeIcon icon={faPhone} />
+                            &nbsp; +55 (11) 1234-5678
+                        </p>
+                        <p>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                            &nbsp; contato@empresa.com
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-        </div>
-    )
-}
+    );
+};
+
+export default Footer;
