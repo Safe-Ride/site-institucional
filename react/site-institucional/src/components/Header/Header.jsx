4c02b4dd481b50458b108d4ab0634e3bcc1e6d1a
daniloBritoo <danilo.brito@sptech.school>
2024-06-09 14:28:21 -0300

    refactor: correção responsividade tela home

--- react/site-institucional/src/components/Header/Header.jsx
+++ react/site-institucional/src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import styles from "./Header.module.css";
 import imgLogo from "../../utils/assets/logo.jpg";
+import { useNavigate } from "react-router-dom";
+import Home from "../../pages/home/Home";
 
 const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setShowMenu(!showMenu);
@@ -14,7 +17,7 @@ const Header = () => {
             <div className={styles["container"]}>
                 <nav className={styles["navbar"]}>
                     <div className={styles["logo"]}>
-                        <img src={imgLogo} alt="Logo" />
+                       <a onClick={() => navigate (`/`)}> <img src={imgLogo} alt="Logo" /></a>
                     </div>
                     <button className={styles["menu-icon"]} onClick={toggleMenu}>
                         <span></span>
@@ -22,9 +25,9 @@ const Header = () => {
                         <span></span>
                     </button>
                     <ul className={`${styles["list_header"]} ${showMenu ? styles["show"] : ""}`}>
-                        <li><a href="#">Pagina Inicial</a></li>
+                        <li><a onClick={() => navigate(`/`)}>Pagina Inicial</a></li>
                         <li><a href="#">Sou Motorista</a></li>
-                        <li><a href="#">Sou Responsável</a></li>
+                        <li><a onClick={() => navigate(`/responsavel`)}>Sou Responsável</a></li>
                         <li><a href="#">Entrar</a></li>
                     </ul>
                 </nav>
